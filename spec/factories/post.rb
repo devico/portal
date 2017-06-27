b7a1f4bdb29d1e8175980298e67c63b68a0c489d
devico <clamdm@mail.ru>
2017-06-27 12:49:49 +0300

    initial commit

---
+++ spec/factories/post.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+
+  factory :post do
+    sequence(:title) { |n| "Post #{n}" }
+    #item.body 'and the reason why the data is the same for each instance'
+    body { Forgery(:lorem_ipsum).words(5) }
+  end
+end
